58a1c70de2b6fbe19f157b3639cd8bfcb589f482
umentu <umentu@gmail.com>
2016-02-07 03:08:50 +0900

    add requirements.txt

--- create_data.py
+++ create_data.py
@@ -2,7 +2,7 @@
 
 import random
 import os
-
+import numpy as np
 
 class CreateData(object):
 
@@ -13,11 +13,24 @@ class CreateData(object):
     def create_number_data(self,
                            file_path,
                            count,
+                           min_int=0,
                            max_int=10000):
 
         with open(file_path, "w") as f:
             for n in range(count):
-                f.write(str(n) + "\t" + str(random.randint(0, max_int)) + "\n")
+                f.write(str(n) + "\t" + str(random.randint(min_int, max_int) + (n * 6)) + "\n")
+
+    def create_line_data(self,
+                        file_path,
+                        count):
+
+       with open(file_path, "w") as f:
+            for n in range(count):
+                f.write(str(n)
+                         + "\t"
+                         + str(n * n + random.randint(0, 100000))
+                         + "\n")
+
 
 
 if __name__ == '__main__':
@@ -26,6 +39,7 @@ if __name__ == '__main__':
     create_data.create_number_data(
         os.path.dirname(os.path.abspath(__file__))
          + "/number_data.txt",
-        10000,
-        10000
+        count=1000,
+        min_int=1000,
+        max_int=3000
     )
